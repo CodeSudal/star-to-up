eec28462c16d5e170f50e445f787f6916a068b21
99nighty <117559858+99nighty@users.noreply.github.com>
2023-02-28 11:29:34 +0900

    productDAO 작성중
    1

--- src/main/java/com/startoup/biz/product/ProductDAO.java
+++ src/main/java/com/startoup/biz/product/ProductDAO.java
@@ -8,4 +8,15 @@ import org.springframework.stereotype.Repository;
 public class ProductDAO {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
+	
+	// 제품추가
+	final String INSERT_PRODUCT = "INSERT INTO PRODUCT( P_NUM, P_NAME, P_PRICE, P_INFO, P_IMAGE) VALUES( (SELECT NVL(MAX(P_NUM), 0) + 1 FROM PRODUCT), ?, ?, ?, ?)";
+	// 제품정보변경
+	final String UPDATE_PRODUCT = "";
+	// 제품삭제
+	final String DELETE_PRODUCT = "DELETE FROM PRODUCT WHERE P_NUM=?";
+	// 전체 제품 출력
+	final String SELECT_ALL_PRODUCT = "SELECT * FROM PRODUCT";
+	
+	
 }
