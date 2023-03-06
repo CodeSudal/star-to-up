e2ee9ba601ce6b7cdedbdf5eb08e1a184bc4fc03
KRLJH <101416494+KRLJH@users.noreply.github.com>
2023-03-06 13:41:52 +0900

    임시임 풀 XX
    임시임 풀 XXXXXXXXXX

--- src/main/java/com/startoup/biz/sms/SMSDAO.java
+++ src/main/java/com/startoup/biz/sms/SMSDAO.java
@@ -20,16 +20,24 @@ public class SMSDAO {
 
 		int randNum = (int)(Math.random()*(9000))+1000; // 4자리 인증번호
 
-		String api_key = ""; // 테스트시 가입 후 입력
-		String api_secret = ""; // 테스트시 가입 후 입력
+		String api_key = "NCSYJJBPPFN2BFTA"; // 테스트시 가입 후 입력
+		String api_secret = "4JTYBBKMSVRNBV6ZYESOPVAQ8ZELSJ4F"; // 테스트시 가입 후 입력
 		Message coolsms = new Message(api_key , api_secret);   
 
 		HashMap<String, String> params = new HashMap<String, String>();
-		params.put("to",svo.getsNum() ); // 수신번호
-		params.put("from","01012345678"); // 발신번호
-		params.put("type","sms"); // SMS의 문자
-		params.put("text","Coolsms Testing Message! "+randNum); // 문자내용
-		params.put("app_version", "test app 1.2"); // 쿨SMS버전인듯
+		
+		// 4 params(to, from, type, text) are mandatory. must be filled
+        HashMap<String, String> params = new HashMap<String, String>();
+        params.put("to", userPhoneNumber);    // 수신전화번호
+        params.put("from", "01094618018");    // 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
+        params.put("type", "SMS");
+        params.put("text", "[TEST] 인증번호는" + "["+randomNumber+"]" + "입니다."); // 문자 내용 입력
+        params.put("app_version", "test app 1.2"); // application name and version
+//		params.put("to",svo.getsNum() ); // 수신번호
+//		params.put("from","01012345678"); // 발신번호
+//		params.put("type","sms"); // SMS의 문자
+//		params.put("text","Coolsms Testing Message! "+randNum); // 문자내용
+//		params.put("app_version", "test app 1.2"); // 쿨SMS버전인듯
 
 		try {
 			JSONObject obj = (JSONObject) coolsms.send(params);
@@ -38,6 +46,6 @@ public class SMSDAO {
 			System.out.println(e.getMessage());
 			System.out.println(e.getCode());
 		}
-		return randNum;
+		//return randNum;
 	}
 }
