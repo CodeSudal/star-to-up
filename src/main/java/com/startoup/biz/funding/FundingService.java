36b9dc341cd6d8815be67ea660f7300df5a8edaa
99nighty <117559858+99nighty@users.noreply.github.com>
2023-03-06 11:38:56 +0900

    Funding Product 통합 수정
    1

--- src/main/java/com/startoup/biz/funding/FundingService.java
+++
@@ -1,5 +0,0 @@
-package com.startoup.biz.funding;
-
-public interface FundingService {
-
-}
--- src/main/java/com/startoup/biz/product/ProductDAO.java
+++ src/main/java/com/startoup/biz/product/ProductDAO.java
@@ -15,31 +15,89 @@ public class ProductDAO {
 	private JdbcTemplate jdbcTemplate;
 
 	// 제품추가
-	final String INSERT_PRODUCT = "INSERT INTO PRODUCT( P_NUM, P_NAME, P_PRICE, P_INFO, P_IMAGE) VALUES( (SELECT NVL(MAX(P_NUM), 0) + 1 FROM PRODUCT), ?, ?, ?, ?)";
+	final String INSERT_PRODUCT = "INSERT INTO PRODUCT( P_NUM, P_NAME, P_PRICE, P_INFO, P_IMAGE, P_AMOUNT) VALUES( (SELECT NVL(MAX(P_NUM), 0) + 1 FROM PRODUCT), ?, ?, ?, ?, ?)";
 	// 제품정보변경
-	final String UPDATE_PRODUCT = "UPDATE PRODUCT SET P_NAME=?, P_PRICE=?, P_INFO=?. P_IMAGE=? WHERE P_NUM=?;";
-	// 펀딩 완료 시(제품 펀딩 종료하기)
-	final String UPDATE_FINISH = "UPDATE PRODUCT SET P_FINISH=1 WHERE P_NUM=?;";
+	final String UPDATE_PRODUCT = "UPDATE PRODUCT SET P_NAME=?, P_PRICE=?, P_INFO=?, P_IMAGE=?, P_AMOUNT=? WHERE P_NUM=?";
 	// 제품삭제
 	final String DELETE_PRODUCT = "DELETE FROM PRODUCT WHERE P_NUM=?";
 	// 전체 제품 출력(SELECT ALL)
-	final String SELECT_ALL_PRODUCT = "SELECT P_NUM, P_NAME, P_PRICE, P_INFO, P_FINISH, P_IMAGE FROM PRODUCT";
+	final String SELECT_ALL_PRODUCT = "SELECT P_NUM, P_NAME, P_PRICE, P_INFO, P_FINISH, P_IMAGE, P_AMOUNT, P_CRNAMOUNT FROM PRODUCT";
 	// 제품 하나 출력(SELECT ONE)
-	final String SELECT_ONE_PRODUCT = "SELECT P_NUM, P_NAME, P_PRICE, P_INFO, P_FINISH, P_IMAGE FROM PRODUCT WHERE P_NUM=?";
-	// 제품 펀딩 확인 기능(펀딩 진행중 0, 끝났으면 1), checkEnd
-	final String SELECT_ONE_FINISH = "SELECT P_FINISH FROM PRODUCT WHERE P_NUM=?";
+	final String SELECT_ONE_PRODUCT = "SELECT P_NUM, P_NAME, P_PRICE, P_INFO, P_FINISH, P_IMAGE, P_AMOUNT, P_CRNAMOUNT FROM PRODUCT WHERE P_NUM=?";
 	
-	// 사용법)
-	// insertProduct 메서드 쓸 때 바로 insertFunding 메서드 실행해주면 됨
-	// 그러면 자동으로 product에 해당하는 funding 정보 생성
 	
-// FINISH 두개 수정필요
+	// 펀딩 완료 되었는지 확인
+	final String CHECK_FINISH = "SELECT F_AMOUNT, F_CRNAMOUNT FROM FUNDING WHERE F_PID = ?";
+	// 펀딩 완료 시(제품 펀딩 종료하기)
+	final String UPDATE_FINISH = "UPDATE PRODUCT SET P_FINISH=1 WHERE P_NUM=?";
+	
+	// 사용법
+	// checkEnd() == 사용자가 구매할 때 마다 실행시켜주면 됨
+	
+	// 펀딩 목록 추가
+	final String INSERT_FUNDING = "INSERT INTO FUNDING(F_NUM, F_PID, F_AMOUNT) VALUES( (SELECT NVL(MAX(F_NUM), 0) + 1 FROM FUNDING), (SELECT NVL(MAX(P_NUM), 0) FROM PRODUCT), ?);";
+	// 펀딩 상태 변경
+	final String UPDATE_FUNDING = "UPDATE FUNDING SET F_CRNAMOUNT=((SELECT F_CRNAMOUNT FROM FUNDING WHERE F_PID=?) + (SELECT P_PRICE FROM PRODUCT WHERE P_NUM=?)) WHERE F_PID=?";
+	// 펀딩 전체 목록 보기
+	final String SELECT_ALL_FUNDING = "SELECT F_NUM, F_PID, F_AMOUNT, F_CRNAMOUNT FROM FUNDING";
+	// 펀딩 목록 하나 보기
+	final String SELECT_ONE_FUNDING = "SELECT F_NUM, F_PID, F_AMOUNT, F_CRNAMOUNT FROM FUNDING WHERE F_NUM=?";
+
+	public boolean insertFunding(ProductVO vo) {
+		try {
+			int res=jdbcTemplate.update(INSERT_FUNDING, vo.getpAmount());
+			if(res<1) { return false; }
+			return true;
+		} catch(Exception e) {
+			return false;
+		}
+	}
+
+	public boolean updateFunding(ProductVO vo) {
+		try {
+			int res=jdbcTemplate.update(UPDATE_FUNDING, vo.getpNum(), vo.getpNum(), vo.getpNum());
+			if(res<1) { return false; }
+			return true;
+		} catch(Exception e) {
+			return false;
+		}
+	}
+
+	public List<ProductVO> selectAllFunding(ProductVO vo){
+		try {
+			return jdbcTemplate.query(SELECT_ALL_FUNDING, new ProductRowMapper());
+		} catch(Exception e) {
+			return null;
+		}
+	}
+
+	public ProductVO selectOneFunding(ProductVO vo) {
+		Object[] args= { vo.getpNum() };
+		try {
+			return jdbcTemplate.queryForObject(SELECT_ONE_FUNDING, args, new ProductRowMapper());
+		} catch(Exception e) {
+			return null;
+		}
+	}
+	
+	// 펀딩 진행도 (퍼센트)
+	public int progressFunding(ProductVO vo) {
+		Object[] args= { vo.getpNum() };
+		try {
+			int res=0;
+			ProductVO pvo = jdbcTemplate.queryForObject(SELECT_ONE_FUNDING, args, new ProductRowMapper());
+			res=(pvo.getpAmount()/pvo.getpCRNAmount())*100;
+			return res;
+		} catch(Exception e) {
+			return 0;
+		}
+	}
 
 	public boolean insertProduct(ProductVO vo) {
 		try {
 			int res=jdbcTemplate.update(INSERT_PRODUCT, vo.getpName(), vo.getpPrice(), vo.getpInfo(), vo.getpImage());
-			System.out.println(res);
 			if(res<1) { return false; }
+			jdbcTemplate.update(INSERT_FUNDING, vo.getpAmount());
 			return true;
 		} catch(Exception e) {
 			return false;
@@ -56,15 +114,6 @@ public class ProductDAO {
 		}
 	}
 	
-	public boolean updateFinish(ProductVO vo) {
-		try {
-			int res=jdbcTemplate.update(UPDATE_FINISH, vo.getpNum());
-			if(res<1) { return false; }
-			return true;
-		} catch(Exception e) {
-			return false;
-		}
-	}
 	public boolean deleteProduct(ProductVO vo) {
 		try {
 			int res=jdbcTemplate.update(DELETE_PRODUCT, vo.getpNum());
@@ -75,7 +124,7 @@ public class ProductDAO {
 		}
 	}
 
-	public List<ProductVO> selectAll(ProductVO vo){
+	public List<ProductVO> selectAllProduct(ProductVO vo){
 		try {
 			return jdbcTemplate.query(SELECT_ALL_PRODUCT, new ProductRowMapper());
 		} catch(Exception e) {
@@ -83,7 +132,7 @@ public class ProductDAO {
 		}
 	}
 
-	public ProductVO selectOne(ProductVO vo) {
+	public ProductVO selectOneProduct(ProductVO vo) {
 		Object[] args= { vo.getpNum() };
 		try {
 			return jdbcTemplate.queryForObject(SELECT_ONE_PRODUCT, args, new ProductRowMapper());
@@ -96,13 +145,17 @@ public class ProductDAO {
 	public boolean checkEnd(ProductVO vo) {
 		Object[] args= { vo.getpNum() };
 		try {
-			jdbcTemplate.queryForObject(SELECT_ONE_FINISH, args, new ProductRowMapper());
-			int res=vo.getpFinish();
-			if(res==0) { return true; }
+			jdbcTemplate.queryForObject(CHECK_FINISH, args, new ProductRowMapper());
+			if(vo.getpAmount()==vo.getpCRNAmount()) {
+				jdbcTemplate.update(UPDATE_FINISH, vo.getpNum());
+				return true;
+			}
+			else {
+				return true;
+			}
 		} catch(Exception e) {
 			return false;
 		}
-		return false;
 	}
 
 
@@ -123,3 +176,39 @@ class ProductRowMapper implements RowMapper<ProductVO> {
 	}
 
 }
+
+//class FundingRowMapper implements RowMapper<FundingVO> {
+//
+//	@Override
+//	public FundingVO mapRow(ResultSet rs, int rowNum) throws SQLException {
+//		FundingVO data=new FundingVO();
+//		data.setfNum(rs.getInt("F_NUM"));
+//		data.setfPid(rs.getInt("F_PID"));
+//		data.setfCRNAmount(rs.getInt("F_AMOUNT"));
+//		data.setfAmount(rs.getInt("F_CRNAMOUNT"));
+//		return data;
+//	}
+//
+//}
+//
+//class PFundingRowMapper implements RowMapper<PFundingVO> {
+//
+//	@Override
+//	public PFundingVO mapRow(ResultSet rs, int rowNum) throws SQLException {
+//		PFundingVO data=new PFundingVO();
+//		data.setpNum(rs.getInt("P_NUM"));
+//		data.setpName(rs.getString("P_NAME"));
+//		data.setpPrice(rs.getInt("P_PRICE"));
+//		data.setpInfo(rs.getString("P_INFO"));
+//		data.setpFinish(rs.getInt("P_FINISH"));
+//		data.setpImage(rs.getString("P_IMAGE"));
+//		
+//		data.setfNum(rs.getInt("F_NUM"));
+//		data.setfPid(rs.getInt("F_PID"));
+//		data.setfCRNAmount(rs.getInt("F_AMOUNT"));
+//		data.setfAmount(rs.getInt("F_CRNAMOUNT"));
+//		return data;
+//	}
+// 	
+//}
+
