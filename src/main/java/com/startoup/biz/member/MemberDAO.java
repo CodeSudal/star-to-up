1d308e933315ed244441d9579aa9bbcc134040ca
99nighty <117559858+99nighty@users.noreply.github.com>
2023-03-07 14:56:48 +0900

    셀레니움 디펜던시, mdao, webdriver
    1

--- src/main/java/com/startoup/biz/member/MemberDAO.java
+++ src/main/java/com/startoup/biz/member/MemberDAO.java
@@ -9,7 +9,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
-@Repository("memberDAO")
 public class MemberDAO {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
