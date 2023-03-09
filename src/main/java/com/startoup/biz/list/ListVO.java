7470382984afc03d96c2e90586d3c2ecadd7f4ca
99nighty <117559858+99nighty@users.noreply.github.com>
2023-03-09 15:47:06 +0900

    List 수정
    1

--- src/main/java/com/startoup/biz/list/ListVO.java
+++ src/main/java/com/startoup/biz/list/ListVO.java
@@ -4,6 +4,7 @@ public class ListVO {
 	private int lNum;	// 펀딩리스트 번호
 	private int lPid;	// 제품번호
 	private String lMid;	// 아이디
+	private int lCnt;	// 인원 수
 	
 	public int getlNum() {
 		return lNum;
@@ -22,6 +23,13 @@ public class ListVO {
 	}
 	public void setlMid(String lMid) {
 		this.lMid = lMid;
+	}
+	
+	public int getlCnt() {
+		return lCnt;
+	}
+	public void setlCnt(int lCnt) {
+		this.lCnt = lCnt;
 	}
 	@Override
 	public String toString() {
