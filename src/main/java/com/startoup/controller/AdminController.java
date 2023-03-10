bda430d152c4286aaf597b00b71d7a7646ec8e4e
kang <122261115+wonjiwonji@users.noreply.github.com>
2023-03-10 15:52:58 +0900

    crawling 연결

--- src/main/java/com/startoup/controller/AdminController.java
+++ src/main/java/com/startoup/controller/AdminController.java
@@ -7,17 +7,46 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.startoup.biz.member.MemberServiceImpl;
 import com.startoup.biz.member.MemberVO;
+import com.startoup.biz.product.ProductServiceImpl;
+import com.startoup.biz.product.ProductVO;
 
 @Controller
 public class AdminController {
 
 	@Autowired
 	private MemberServiceImpl memberSI;
+	@Autowired
+	private ProductServiceImpl ProductSI;
+	
+	@RequestMapping(value="/adminFunding.do")
+	public String selectAllFund(ProductVO pvo, Model model) {
+		
+		model.addAttribute("fundingList", ProductSI.selectAll(pvo));
+		return "adminFundingManagement.jsp";
+	}
+	
+	@RequestMapping(value="/adminProduct.do")
+	public String selectAllProduct(ProductVO pvo, Model model) {
+		
+		model.addAttribute("datas", ProductSI.selectAll(pvo));
+		return "adminProductManagement.jsp";
+	}
 	
 	@RequestMapping(value="/adminMember.do")
 	public String selectAllMember(MemberVO mvo, Model model) {
 		
-		model.addAttribute("datas", memberSI.selectAllMember(mvo));
+		model.addAttribute("memberList", memberSI.selectAllMember(mvo));
 		return "adminMemberManagement.jsp";
 	}
+	
+	@RequestMapping(value="/adminMemberDelete.do")
+	public String deleteMember(MemberVO mvo, Model model) {
+		
+		model.addAttribute("memberList", memberSI.deleteMember(mvo));
+		return "redirect:adminMember.do";
+	}
+	
+	
+	
+	
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,7 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%
-	response.sendRedirect("main.do");
+	response.sendRedirect("crawling.do");
+/* 	response.sendRedirect("main.do"); */
 %>
 <!-- .jsp 가 아닌 .do로 하는 이유
 1. 아주 작은 데이터라도 컨트롤러를 거치려면 .do로 써야하고
