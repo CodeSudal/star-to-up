6f14a1dae496a78c0d487e8b336485139b2f85b4
99nighty <117559858+99nighty@users.noreply.github.com>
2023-03-15 14:30:16 +0900

    Update CrawlingDAO.java
    태양계 행성이 아니라면 한글 이름 출력

--- src/main/java/com/startoup/biz/crawling/CrawlingDAO.java
+++ src/main/java/com/startoup/biz/crawling/CrawlingDAO.java
@@ -20,21 +20,27 @@ public class CrawlingDAO {
 	
 	// 상품 확인
 	final String SELECT_ALL_PRODUCT = "SELECT P_NAME, P_INFO FROM PRODUCT";
+	// 상품 이름 확인
+	final String SELECT_ALL_PRODUCT2 = "SELECT P_NAME, P_EN FROM PRODUCT";
 	// 크롤링 정보 DB에 저장
 	final String INSERT_CRAWLING = "INSERT INTO CRAWLING( C_NUM, C_NAME, C_INFO) VALUES( (SELECT NVL(MAX(C_NUM), 0) + 1 FROM CRAWLING), ?, ?)";
 	// 상품 INFO 업데이트
 	final String UPDATE_PINFO = "UPDATE PRODUCT P SET P.P_INFO = (SELECT C.C_INFO FROM CRAWLING C WHERE P.P_NAME = C.C_NAME)";
+	// 상품 INFO 업데이트2
+	final String UPDATE_PINFO2 = "UPDATE PRODUCT P SET P.P_INFO = (SELECT C.C_INFO FROM CRAWLING C WHERE P.P_NAME = ?)";
 	// SELECTONE
 	final String SELECT_ONE_CRAWLING = "SELECT C_NUM, C_NAME, C_INFO FROM CRAWLING WHERE C_NAME = ?";
 	// SELECTALL
 	final String SELECT_ALL_CRAWLING = "SELECT C_NUM, C_NAME, C_INFO FROM CRAWLING";
 	
 	// NAMING SELECTALL
-	final String SELECT_ALL_NAMING = "SELECT N_NUM, N_NAME, N_EN FROM NAMING";
+	final String SELECT_ALL_NAMING = "SELECT N_NAME, N_EN FROM NAMING";
 	// INSERT NAMING
 	final String INSERT_NAMING = "INSERT INTO NAMING(N_NUM, N_NAME, N_EN) VALUES((SELECT NVL(MAX(N_NUM), 0) + 1 FROM NAMING), ?, ?)";
 	// 제품 NAME 업데이트
 	final String UPDATE_PNAME = "UPDATE PRODUCT P SET P.P_EN = (SELECT N.N_EN FROM NAMING N WHERE P.P_NAME = N.N_NAME)";
+	// 제품 NAME 업데이트2
+	final String UPDATE_PNAME2 = "UPDATE PRODUCT SET P_EN = ? WHERE P_NAME = ?";
 	
 	public boolean crawling() {
 		// DB(크롤링 테이블)에 저장해줄 sel 객체
@@ -47,16 +53,30 @@ public class CrawlingDAO {
 		String title;
 		// 셀레니움 크롤링 대상 정보
 		String info;
+		// 네이밍을 한글로 해줄 LIST
+		List<NamingVO> ndb;
+		// 개수 확인
+		List<NamingVO> nList;
 
 		try {
-			insertNaming();
+			nList=jdbcTemplate.query(SELECT_ALL_NAMING, new NamingRowMapper());
+			if(nList.size()<9) { insertNaming(); }
+			
 			jdbcTemplate.update(UPDATE_PNAME);
+					
+			ndb=jdbcTemplate.query(SELECT_ALL_PRODUCT2, new PNamingRowMapper());
+			
+			System.out.println(ndb);
+			
+			for(int i=0; i<ndb.size(); i++) {
+				if(ndb.get(i).getnEN()==null) {
+					jdbcTemplate.update(UPDATE_PNAME2, ndb.get(i).getnName(), ndb.get(i).getnName());
+				}
+			}
 			// 이미 크롤링 데이터가 9개 이상이라면 크롤링 Skip
 			cdb=jdbcTemplate.query(SELECT_ALL_CRAWLING, new CSeleniumRowMapper());
-			if(cdb.size()>8) { return true; }
 			
-			// 셀레니움 크롤링 함수 호출
-			DriverUtil.crawling();
+			if(cdb.size()<9) { DriverUtil.crawling(); }
 			
 			// forEach문을 돌면서 크롤링 정보가 담긴 map에 있는 정보들을
 			// DB에 저장
@@ -79,10 +99,10 @@ public class CrawlingDAO {
 			for(int i=0; i<datas.size(); i++) {
 				title=datas.get(i).getpName();
 				info=DriverUtil.craw.get(title);
+				
 				if(title==null || info==null) { continue; }
 				sel.setcName(title);
-				sel.setcInfo(info);
-				jdbcTemplate.update(UPDATE_PINFO);
+				jdbcTemplate.update(UPDATE_PINFO2, sel.getcName());
 			}
 			return true;
 		} catch(Exception e) {
@@ -140,12 +160,23 @@ class CSeleniumRowMapper implements RowMapper<SeleniumVO> {
 
 }
 
+class PNamingRowMapper implements RowMapper<NamingVO> {
+
+	@Override
+	public NamingVO mapRow(ResultSet rs, int rowNum) throws SQLException {
+		NamingVO data=new NamingVO();
+		data.setnName(rs.getString("P_NAME"));
+		data.setnEN(rs.getString("P_EN"));
+		return data;
+	}
+
+}
+
 class NamingRowMapper implements RowMapper<NamingVO> {
 
 	@Override
 	public NamingVO mapRow(ResultSet rs, int rowNum) throws SQLException {
 		NamingVO data=new NamingVO();
-		data.setnNum(rs.getInt("N_NUM"));
 		data.setnName(rs.getString("N_NAME"));
 		data.setnEN(rs.getString("N_EN"));
 		return data;
