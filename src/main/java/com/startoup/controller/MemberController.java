695bf536fdb7ae9b080fb7fa5b6dd390be44eea4
kang <122261115+wonjiwonji@users.noreply.github.com>
2023-03-16 15:21:03 +0900

    카카오 회원탈퇴, 알럿창

--- src/main/java/com/startoup/controller/MemberController.java
+++ src/main/java/com/startoup/controller/MemberController.java
@@ -43,12 +43,10 @@ public class MemberController {
 	// 약관 동의
 	@RequestMapping(value = "/agreement.do")
 	public String agreement() {
-
 		return "agreement.jsp";
 	}
 
-	// 로그인 페이지로 처음 이동할 때
-
+	// Id 중복 체크
 	@RequestMapping(value = "/checkId.do", method = RequestMethod.POST)
 	public @ResponseBody String checkId(@RequestBody MemberVO vo) {
 
@@ -58,9 +56,9 @@ public class MemberController {
 		} else {
 			return "duplicate";
 		}
-
 	}
 
+	// 이메일 중복 체크
 	@ResponseBody
 	@RequestMapping(value = "/checkEmail.do", method = RequestMethod.POST)
 	public String checkEmail(MemberVO vo) {
@@ -77,112 +75,128 @@ public class MemberController {
 	// 로그인 페이지로 처음 이동할 때
 	@RequestMapping(value = "/login.do", method = RequestMethod.GET)
 	public String loginView(@ModelAttribute("member") MemberVO vo) {
-
 		return "login.jsp";
 	}
 
 	// 로그인 했을 때
 	@RequestMapping(value = "/login.do", method = RequestMethod.POST)
-	public String selectOneMember(MemberVO vo, MyLikeVO mlvo, Model model, String msg, HttpSession session) {
+	public String selectOneMember(MemberVO vo, MyLikeVO mlvo, Model model, String msg, String lo, HttpSession session) {
 		vo = memberSI.loginMember(vo);
 		System.out.println("login : " + vo);
 
 		if (vo == null) { // 로그인 실패
-			// 로그인 실패
-			System.out.println("로그: 로그인 실패");
 
-			msg = "아이디 또는 비밀번호를 잘못 입력하셨습니다.";
+			// alert.jsp
+			msg = "loginFalse";
 			model.addAttribute("msg", msg);
 			return "alert.jsp";
 
-//         return "redirect:login.do"; // 다시 로그인 페이지
 		} else { // 로그인 성공 하면
 			mlvo.setMlMid(vo.getmId()); // 아이디 mlvo에 저장
 			vo.setmLikecnt(memberSI.countLike(mlvo).getMlNum());// likecnt 쿼리문 돌려서 저장
 			session.setAttribute("member", vo); // 세션에 정보 저장 "member"라는 이름으로
-			
+
 			return "redirect:store.do";
 		}
 	}
 
+	// 카카오 로그인
 	@RequestMapping(value = "/kakaoLogin.do")
-	public String kakaoMember(MemberVO vo,MyLikeVO mlvo, HttpSession session) {
-
-//         System.out.println("로그 kakao 확인 id:"+vo.getmId()+"/mName:"+vo.getmName()+"/mEmail1:"+vo.getmEmail1());
+	public String kakaoMember(MemberVO vo, MyLikeVO mlvo,HttpSession session) {
 
 		// 처음 카카오로그인한 계정이라면, 회원가입
-		if (memberSI.selectOneMember(vo) == null) {
+		if (memberSI.selectKakaoMember(vo) == null) {
 			memberSI.insertKakaoMember(vo);
 		}
+		vo = memberSI.selectKakaoMember(vo);
 		mlvo.setMlMid(vo.getmId()); // 아이디 mlvo에 저장
 		vo.setmLikecnt(memberSI.countLike(mlvo).getMlNum());// likecnt 쿼리문 돌려서 저장
 		session.setAttribute("member", vo); // 세션에 정보 저장 "member"라는 이름으로
+		System.out.println(vo);
 		return "redirect:store.do";
 	}
 
 	// 로그아웃 했을 때
 	@RequestMapping(value = "/logout.do")
-	public String logoutMember(HttpSession session) {
+	public String logoutMember(HttpSession session, String msg,String lo, Model model) {
 		System.out.println("logoutMember() 입장");
 
 		session.removeAttribute("mylike");
 		session.removeAttribute("member"); // 세션 특정 정보만 비우기
 
-		return "redirect:login.do";
+		msg = "logout";
+		lo = "main.do";
+		model.addAttribute("msg", msg);
+		model.addAttribute("lo", lo);
+		return "alert.jsp";
 	}
 
+	// 회원가입 화면
 	@RequestMapping(value = "/join.do", method = RequestMethod.GET)
 	public String joinView() {
-
 		return "join.jsp";
 	}
 
+	// 회원가입 버튼 클릭 시
 	@RequestMapping(value = "/join.do", method = RequestMethod.POST)
-	public String insertMember(MemberVO vo) {
+	public String insertMember(MemberVO vo, String msg, String lo, Model model) {
 		System.out.println("insertMember() 입장");
 
-		memberSI.insertMember(vo);
-
-		return "redirect:login.do";
+		if (!memberSI.insertMember(vo)) {
+			msg = "joinFalse";
+			model.addAttribute("msg", msg);
+		} else {
+			msg = "joinTrue";
+			lo = "login.do";
+			model.addAttribute("msg", msg);
+		}
+		return "alert.jsp";
 	}
 
-	// 회원탈퇴 페이지로
+	// 회원 탈퇴 페이지로
 	@RequestMapping(value = "/withdrowal.do")
 	public String withdrowalView() {
 		return "withdrowal.jsp";
 	}
-	
-	// 회원탈퇴 버튼 누름
-	@RequestMapping(value = "/deleteMember.do")
-	public String deletemember(MemberVO vo, HttpSession session) {
-		System.out.println("deleteMember() 입장");
-		MemberVO member = (MemberVO) session.getAttribute("member");
-		vo.setmId(member.getmId());
-		memberSI.deleteMember(vo); // deleteMemter session.removeAttribute("member");
-		System.out.println("db 삭제 성공");
-		session.removeAttribute("member");
-		System.out.println("session 비워짐");
-		return "store.do";
+
+	// 카카오 회원 탈퇴 페이지로
+	@RequestMapping(value = "/withdrowal2.do")
+	public String withdrowal2View() { // kakao withdrowal
+		return "withdrowal2.jsp";
 	}
 
-	
-	// 비밀번호 확인
+	// 회원탈퇴 버튼 누름
+	@RequestMapping(value = "/deleteMember.do")
+	public String deletemember(MemberVO vo, HttpSession session, String msg, String lo, Model model) {
+		MemberVO member = (MemberVO) session.getAttribute("member");
+		vo.setmId(member.getmId());
+		if (!memberSI.deleteMember(vo)) {
+			msg = "withdrowalFalse";
+			model.addAttribute("msg", msg);
+		} else {
+			msg = "withdrowalTrue";
+			lo = "main.do";
+			model.addAttribute("msg", msg);
+			model.addAttribute("lo", lo);
+			session.removeAttribute("member");
+		}
+		return "alert.jsp";
+	}
+
+	// 회원탈퇴 - 비밀번호 확인
 	@ResponseBody
 	@RequestMapping(value = "/pwChk.do")
 	public String pwChk(MemberVO vo, HttpSession session) {
-		
 		MemberVO member = (MemberVO) session.getAttribute("member");
+
 		vo.setmPw(vo.getmPw());
+
 		if (member.getmPw().equals(vo.getmPw())) {
 			return "true";
 		} else {
 			return "0";
 		}
-		
-
 	}
-
-	
 
 	// 상품디테일 페이지 들어갔을 때 찜 여부 확인하기위함.
 	@RequestMapping(value = "/detail.do")
@@ -217,49 +231,27 @@ public class MemberController {
 		} else {
 			return "login.jsp"; // 로그인 하세요 메세지 띄우기
 		}
-		System.out.println(memberSI.myLikeList(mlvo));
 		if (memberSI.myLikeList(mlvo).isEmpty()) {
-			System.out.println("찜한 목록이 없습니다. 화면 보여주기");
-			return "shopcart.jsp";
+			return "noProduct.jsp";
 		}
-		System.out.println("ctrl의 return받아온값" + memberSI.myLikeList(mlvo));
 		model.addAttribute("datas", memberSI.myLikeList(mlvo));
 
 		return "shopcart.jsp";
 
 	}
 
-	/*
-	 * // 찜목록 - 삭제
-	 * 
-	 * @RequestMapping(value = "/shopcartDelete.do", method = RequestMethod.POST)
-	 * 
-	 * @ResponseBody public String deleteLikeList(@RequestBody List objParams, Model
-	 * model, HttpSession session) { //@RequestParam(value="deleteList[]")
-	 * List<Integer> deleteList, List<MyLikeVO> mlist, MyLikeVO mlvo,
-	 * 
-	 * 
-	 * 
-	 * System.out.println("들어옴"+objParams );
-	 * 
-	 * MemberVO mvo = (MemberVO) session.getAttribute("member");
-	 * //mlvo.setMlMid(mvo.getmId()); System.out.println("진입전");
-	 * 
-	 * //model.addAttribute("datas", memberSI.deleteLikeList(mlist, mlvo));
-	 * System.out.println("return 전"); return "true"; }
-	 */
-
 	// 찜목록 - 삭제
 	@RequestMapping(value = "/shopcartDelete.do", method = RequestMethod.POST)
 	@ResponseBody
-	public String deleteLikeList(@RequestBody List<Integer> list, Model model, MyLikeVO mlvo, HttpSession session) {
+	public String deleteLikeList(@RequestBody List<Integer> list, String msg, Model model, MyLikeVO mlvo,
+			HttpSession session) {
 
-		System.out.println("들어옴" + list);
 		MemberVO mvo = (MemberVO) session.getAttribute("member");
 		mlvo.setMlMid(mvo.getmId());
-		System.out.println("진입전");
 		model.addAttribute("datas", memberSI.deleteLikeList(list, mlvo));
-		System.out.println("return 전");
+		mvo.setmLikecnt(memberSI.countLike(mlvo).getMlNum());// likecnt 쿼리문 돌려서 저장
+		session.setAttribute("member", mvo); // 세션에 정보 저장 "member"라는 이름으로
+
 		return "true";
 	}
 
@@ -292,17 +284,24 @@ public class MemberController {
 			return "success";
 		} else {
 			System.out.println("삭제안됨");
-			return "s";
+			return "0";
 		}
 	}
 
+	// 내 펀딩 내역 보기
 	@RequestMapping(value = "/myFundingList.do")
 	public String withdrowalVie(MyFundingVO vo, Model model, HttpSession session) {
-		System.out.println("펀딩 내역 들어옴");
-		MemberVO mvo = (MemberVO) session.getAttribute("member");
-		vo.setMfMid(mvo.getmId());
+		MemberVO member = (MemberVO) session.getAttribute("member");
+		if (member != null) {
+			vo.setMfMid(member.getmId());
+		} else {
+			return "login.jsp"; // 로그인 하세요 메세지 띄우기
+		}
+		if (memberSI.myFundList(vo).isEmpty()) {
+			return "noProduct.jsp";
+		}
 		model.addAttribute("datas", memberSI.myFundList(vo));
-		System.out.println(memberSI.myFundList(vo));
+
 		return "orderList.jsp";
 	}
 
--- src/main/webapp/alert.jsp
+++ src/main/webapp/alert.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,10 +8,34 @@
 </head>
 <body>
 
-<script type="text/javascript">
-	alert('${msg}');
-	location.href = document.referrer;
-	//history.back();
+	<script type="text/javascript">
+	var msg = '${msg}';
+	
+	if(msg == 'loginFalse') {
+		alert('아이디 또는 비밀번호를 잘못 입력하셨습니다.');
+		location.href = document.referrer;
+	} else if (msg == 'logout') {
+		alert('로그아웃 되었습니다.');
+		location.href = '${lo}';
+	} else if (msg == 'joinFalse') {
+		alert('회원가입에 실패하셨습니다. 관리자에게 문의하세요.');
+		location.href = document.referrer;
+	} else if (msg == 'joinTrue') {
+		alert('회원가입을 성공하셨습니다.');
+		location.href = '${lo}';
+	} else if (msg == 'withdrowalFalse') {
+		alert('회원 탈퇴를 실패하셨습니다. 관리자에게 문의하세요.');
+		location.href = document.referrer;
+	} else if (msg == 'withdrowalTrue') {
+		alert('정상적으로 회원 탈퇴 되었습니다.');
+		location.href = '${lo}';
+	} else if (msg == 'paymentSuccess') {
+		alert('결제를 성공하셨습니다! 내 펀딩 목록 페이지로 이동합니다.');
+		location.href = '${lo}';
+	} else {
+		alert('관리자에게 문의해주세요');
+		location.href = document.referrer;
+	}
 </script>
 
 </body>
