ecffa140c14583c174aa09a3b4f8f5f218d704e8
kang <122261115+wonjiwonji@users.noreply.github.com>
2023-03-20 10:52:02 +0900

    0320 오전

--- src/main/java/com/startoup/controller/AdminController.java
+++ src/main/java/com/startoup/controller/AdminController.java
@@ -8,12 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import com.startoup.biz.list.ListServiceImpl;
 import com.startoup.biz.list.ListVO;
 import com.startoup.biz.member.MemberServiceImpl;
@@ -24,139 +22,178 @@ import com.startoup.biz.product.ProductVO;
 @Controller // @Controller + @ResponseBody
 public class AdminController {
 
-	@Autowired
-	private MemberServiceImpl memberSI;
-	@Autowired
-	private ProductServiceImpl ProductSI;
-	@Autowired
-	private ListServiceImpl listSI;
+   @Autowired
+   private MemberServiceImpl memberSI;
+   @Autowired
+   private ProductServiceImpl ProductSI;
+   @Autowired
+   private ListServiceImpl listSI;
+   
+   
+   @RequestMapping(value = "/adminFunding.do")
+   public String selectAllFund(ProductVO pvo, Model model) {
 
-	@RequestMapping(value = "/adminFunding.do")
-	public String selectAllFund(ProductVO pvo, Model model) {
+      model.addAttribute("fundingList", ProductSI.selectAll(pvo));
+      return "adminFundingManagement.jsp";
+   }
+   
+   
+//   @RequestMapping(value = "/adminFunding.do")
+//   public String selectAllFund(ProductVO pvo, Model model) {
+//
+//      model.addAttribute("fundingList", ProductSI.selectAll(pvo));
+//      return "adminFundingManagement.jsp";
+//   }
+   
+   
+   // sendAllMail.jsp 로 이동
+   @RequestMapping(value = "/sendAllMail.do", method = RequestMethod.GET)
+   public String selectAllMailView(ListVO vo,Model model) {
+      
+      System.out.println("   [컨트롤러] sendAllMail Get Controller");
+      
+      // 펀딩한 사람들 이메일, 펀딩한 사람 이름(아이디), 제품명
+      model.addAttribute("fundingList", listSI.selectAllList(vo));
+      
+      
+      return "sendAllMail.jsp";
+   }
+   
+   
+   
+   // sendAllMail내 메일 전송 메서드
+   @RequestMapping(value = "/sendAllMail.do", method = RequestMethod.POST)
+   public String sendAllMail(Model model, ListVO vo) {
+      System.out.println("   [컨트롤러] sendAllMail POST Controller");
 
-		model.addAttribute("fundingList", ProductSI.selectAll(pvo));
-		return "adminFundingManagement.jsp";
-	}
-	
-	@RequestMapping(value = "/sendAllMail.do")
-	public String selectAllMail(ProductVO pvo, Model model) {
+      /*
+       * Gson gson = new Gson(); String json = gson.toJson(myObject); MyObject
+       * myObject2 = gson.fromJson(json, MyObject.class);
+       */
+      System.out.println("listSi : >>>>>>>>>");
+      System.out.println(listSI.selectAllList(vo));
+      
+      model.addAttribute("fundingList", listSI.selectAllList(vo));
+      //JsonObject data = new JsonObject();
+      //data.addProperty("time", "12:00");
+      //obj.add("data", data);
 
-		/* model.addAttribute("fundingList", ProductSI.selectAll(pvo)); */
-		return "sendAllMail.jsp";
-	}
+      return "sendAllMail.jsp";
+   }
 
-	@ResponseBody
-	@RequestMapping("/sendEmailQR.co")
-	public String sendEmailQR(Gson gson, ListVO vo) {
 
-		/*
-		 * Gson gson = new Gson(); String json = gson.toJson(myObject); MyObject
-		 * myObject2 = gson.fromJson(json, MyObject.class);
-		 */
-		JsonObject obj = new JsonObject();
+//   @ResponseBody
+//   @RequestMapping("/sendEmailQR.do")
+//   public String sendEmailQR(Gson gson, ListVO vo) {
+//
+//      /*
+//       * Gson gson = new Gson(); String json = gson.toJson(myObject); MyObject
+//       * myObject2 = gson.fromJson(json, MyObject.class);
+//       */
+//      JsonObject obj = new JsonObject();
+//
+//      listSI.selectAllList(vo);
+//      obj.addProperty("name", vo.getlMid());// id
+//      obj.addProperty("pName", vo.getLpName()); //pName
+//      obj.addProperty("email", vo.getlEmail()); //Email
+//      
+//
+//      //JsonObject data = new JsonObject();
+//      //data.addProperty("time", "12:00");
+//      //obj.add("data", data);
+//
+//      return obj.toString();
+//   }
 
-		listSI.selectAllList(vo);
-		obj.addProperty("name", vo.getlMid());// id
-		obj.addProperty("pName", vo.getLpName()); //pName
-		obj.addProperty("email", vo.getlEmail()); //Email
-		
+   @RequestMapping(value = "/adminProduct.do")
+   public String selectAllProduct(ProductVO pvo, Model model) {
 
-		//JsonObject data = new JsonObject();
-		//data.addProperty("time", "12:00");
-		//obj.add("data", data);
+      model.addAttribute("datas", ProductSI.selectAll(pvo));
+      return "adminProductManagement.jsp";
+   }
 
-		return obj.toString();
-	}
+   @RequestMapping(value = "/adminProductInsert.do")
+   public String selectAllProductInsert(ProductVO pvo, Model model, String msg, String lo,
+         MultipartHttpServletRequest mtfRequest) {
 
-	@RequestMapping(value = "/adminProduct.do")
-	public String selectAllProduct(ProductVO pvo, Model model) {
+      List<MultipartFile> fileList = pvo.getUploadFile();
+      System.out.println("src value : " + pvo.getUploadFile());
 
-		model.addAttribute("datas", ProductSI.selectAll(pvo));
-		return "adminProductManagement.jsp";
-	}
+      // MultipartFile에 업로드 파일이 들어왔는지 확인
+      if (!fileList.isEmpty()) {
+         System.out.println("이미지 업로드 확인");
+      }
 
-	@RequestMapping(value = "/adminProductInsert.do")
-	public String selectAllProductInsert(ProductVO pvo, Model model, String msg, String lo,
-			MultipartHttpServletRequest mtfRequest) {
+      // 파일을 저장할 내 경로
+      String path = "C:\\jiwon1108\\workspace2\\startoup\\src\\main\\webapp\\img\\";
 
-		List<MultipartFile> fileList = pvo.getUploadFile();
-		System.out.println("src value : " + pvo.getUploadFile());
+      // forEach문을 돌면서 순차적으로 이미지를 저장하기 위한 변수 i
+      int i = 0;
 
-		// MultipartFile에 업로드 파일이 들어왔는지 확인
-		if (!fileList.isEmpty()) {
-			System.out.println("이미지 업로드 확인");
-		}
+      for (MultipartFile mf : fileList) {
+         String originFileName = mf.getOriginalFilename(); // 원본 파일 명
+         long fileSize = mf.getSize(); // 파일 사이즈
 
-		// 파일을 저장할 내 경로
-		String path = "C:\\jiwon1108\\workspace2\\startoup\\src\\main\\webapp\\img\\";
+         System.out.println("originFileName : " + originFileName);
+         System.out.println("fileSize : " + fileSize);
 
-		// forEach문을 돌면서 순차적으로 이미지를 저장하기 위한 변수 i
-		int i = 0;
+         if (originFileName.isBlank()) {
+            break;
+         }
 
-		for (MultipartFile mf : fileList) {
-			String originFileName = mf.getOriginalFilename(); // 원본 파일 명
-			long fileSize = mf.getSize(); // 파일 사이즈
+         // 첫번째 이미지는 setpIamge1에 저장
+         if (i == 0) {
+            pvo.setpImage1(originFileName);
+         }
+         // 두번째 이미지
+         else if (i == 1) {
+            pvo.setpImage2(originFileName);
+         }
+         // 세번째 이미지
+         else if (i == 2) {
+            pvo.setpImage3(originFileName);
+         }
 
-			System.out.println("originFileName : " + originFileName);
-			System.out.println("fileSize : " + fileSize);
+         // 파일 업로드 경로 + 파일 이름
+         String safeFile = path + originFileName;
 
-			if (originFileName.isBlank()) {
-				break;
-			}
+         // 다음 이미지 저장으로 가기 위한 i++
+         i++;
+         try {
+            // 해당 경로에 파일 저장
+            mf.transferTo(new File(safeFile));
+         } catch (IllegalStateException e) {
+            e.printStackTrace();
+         } catch (IOException e) {
+            e.printStackTrace();
+         }
+      }
 
-			// 첫번째 이미지는 setpIamge1에 저장
-			if (i == 0) {
-				pvo.setpImage1(originFileName);
-			}
-			// 두번째 이미지
-			else if (i == 1) {
-				pvo.setpImage2(originFileName);
-			}
-			// 세번째 이미지
-			else if (i == 2) {
-				pvo.setpImage3(originFileName);
-			}
+      boolean insert = ProductSI.insertProduct(pvo);
+      if (!insert) {
+         msg = "insertImageFalse";
+         model.addAttribute("msg", msg);
+      } else {
+         msg = "insertImageTrue";
+         lo = "adminProduct.do";
+         model.addAttribute("msg", msg);
+         model.addAttribute("lo", lo);
+      }
+      return "alert.jsp";
+   }
 
-			// 파일 업로드 경로 + 파일 이름
-			String safeFile = path + originFileName;
+   @RequestMapping(value = "/adminMember.do")
+   public String selectAllMember(MemberVO mvo, Model model) {
 
-			// 다음 이미지 저장으로 가기 위한 i++
-			i++;
-			try {
-				// 해당 경로에 파일 저장
-				mf.transferTo(new File(safeFile));
-			} catch (IllegalStateException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+      model.addAttribute("memberList", memberSI.selectAllMember(mvo));
+      return "adminMemberManagement.jsp";
+   }
 
-		boolean insert = ProductSI.insertProduct(pvo);
-		if (!insert) {
-			msg = "insertImageFalse";
-			model.addAttribute("msg", msg);
-		} else {
-			msg = "insertImageTrue";
-			lo = "adminProduct.do";
-			model.addAttribute("msg", msg);
-			model.addAttribute("lo", lo);
-		}
-		return "alert.jsp";
-	}
+   @RequestMapping(value = "/adminMemberDelete.do")
+   public String deleteMember(MemberVO mvo, Model model) {
 
-	@RequestMapping(value = "/adminMember.do")
-	public String selectAllMember(MemberVO mvo, Model model) {
-
-		model.addAttribute("memberList", memberSI.selectAllMember(mvo));
-		return "adminMemberManagement.jsp";
-	}
-
-	@RequestMapping(value = "/adminMemberDelete.do")
-	public String deleteMember(MemberVO mvo, Model model) {
-
-		model.addAttribute("memberList", memberSI.deleteMember(mvo));
-		return "redirect:adminMember.do";
-	}
+      model.addAttribute("memberList", memberSI.deleteMember(mvo));
+      return "redirect:adminMember.do";
+   }
 
 }
--- src/main/java/com/startoup/controller/ProductController.java
+++ src/main/java/com/startoup/controller/ProductController.java
@@ -1,5 +1,10 @@
 package com.startoup.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,60 +23,79 @@ import com.startoup.biz.product.ProductVO;
 
 @Controller
 public class ProductController {
-	
+
 	@Autowired
 	private ProductServiceImpl productSI;
 	@Autowired
 	private MemberServiceImpl memberSI;
 	@Autowired
 	private ListServiceImpl listSI;
-	
+
 	// 스토어 메인 화면
-	@RequestMapping(value="/store.do")
-	public String storeView(@ModelAttribute("product")ProductVO vo, Model model) {
+	@RequestMapping(value = "/store.do")
+	public String storeView(@ModelAttribute("product") ProductVO vo, Model model, HttpServletRequest request,
+			HttpServletResponse response) throws IOException {
+
+		boolean bool = false;
+
+		Cookie[] cookieArr = request.getCookies();
+		if (cookieArr != null) {
+			for (Cookie c : cookieArr) { // 쿠키목록확인
+				if (c.getName().equals("nopopup")) { // "nopopup"쿠키가 있다면 '오늘 하루 팝업창 열지 않기' 와 같은 의미임.
+					bool = true; // bool == true 이면 팝업창 안뜸
+				}
+			}
+		} else { // 쿠키 목록이 비었다면
+			Cookie cookie = new Cookie("testCookie", "myCookie"); // 쿠키 지정 name,value
+			cookie.setMaxAge(60 * 60 * 24); // 쿠키 유효시간 설정 (하루)
+			response.addCookie(cookie); // 사용자 브라우저에 쿠키 추가
+		}
+
+		model.addAttribute("bool", bool); // bool 값 뷰로 넘겨주기 : bool == true면 팝업창 안뜸
+		model.addAttribute("neww", productSI.selectOne(vo)); // 최신상품 selectone한 값 뷰로 넘겨주기
+
 		model.addAttribute("productList", productSI.selectAll(vo));
 		return "store.jsp";
 	}
-	
+
 	// 결제 화면 입장
-	@RequestMapping(value="/payment.do")
+	@RequestMapping(value = "/payment.do")
 	public String selectOne(ProductVO pvo, MemberVO mvo, Model model) {
 		model.addAttribute("product", productSI.selectOne(pvo));
 		model.addAttribute("member", memberSI.selectOneMember(mvo));
 		return "payment.jsp";
 	}
-	
+
 	// 결제 완료
-	@RequestMapping(value="/paymentsuccess.do")
-	public String insertList(ProductVO pvo, ListVO lvo, MyFundingVO myvo, String msg, String lo, Model model,HttpSession session) {
+	@RequestMapping(value = "/paymentsuccess.do")
+	public String insertList(ProductVO pvo, ListVO lvo, MyFundingVO myvo, String msg, String lo, Model model,
+			HttpSession session) {
 		MemberVO mvo = (MemberVO) session.getAttribute("member"); // 세션 정보 가져와 mvo에 저장
 		// mvo의 Id, PW 값을 lvo와 myvo에 저장
 		lvo.setlMid(mvo.getmId());
 		myvo.setMfMid(mvo.getmId());
 		lvo.setlPid(pvo.getpNum());
 		myvo.setMfPid(pvo.getpNum());
-		
+
 		// db 변경
 		model.addAttribute("list", listSI.insertList(lvo)); // 펀딩 내역 추가
 		model.addAttribute("product", productSI.checkProduct(pvo));
 		model.addAttribute("member", memberSI.insertFunding(myvo));
 		/* model.addAttribute("list", productSI.update) */
-		
+
 		msg = "paymentSuccess";
 		lo = "myFundingList.do";
 		model.addAttribute("msg", msg);
 		model.addAttribute("lo", lo);
 		return "alert.jsp";
 	}
-	
-	
-	
-//	@RequestMapping(value="/detail.do")
-//	public String selectAllProductList(@ModelAttribute("product")ProductVO vo, Model model) {
-//		
-//		System.out.println("ㅓㅏㅣㅏㅓㅣㅏㅏㅓㅣㅏㅓㅣㅏㅓ");
-//		model.addAttribute("productList", ProductSI.selectOne(vo));
-//		return "detail.jsp";
-//	}
+
+//   @RequestMapping(value="/detail.do")
+//   public String selectAllProductList(@ModelAttribute("product")ProductVO vo, Model model) {
+//      
+//      System.out.println("ㅓㅏㅣㅏㅓㅣㅏㅏㅓㅣㅏㅓㅣㅏㅓ");
+//      model.addAttribute("productList", ProductSI.selectOne(vo));
+//      return "detail.jsp";
+//   }
 
 }
