8143699217e3c5ef75c44dae7e96eeed5263bcfe
99nighty <117559858+99nighty@users.noreply.github.com>
2023-07-20 12:24:08 +0900

    Update AfterReturningAdvice.java

--- src/main/java/com/startoup/biz/common/AfterReturningAdvice.java
+++ src/main/java/com/startoup/biz/common/AfterReturningAdvice.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 public class AfterReturningAdvice {
 	@AfterReturning(pointcut="PointcutCommon.bPointcut()", returning="obj")
 	public void printLogARA(JoinPoint jp, Object obj) {
-		///// System.out.println("[ARA 로그] 비즈니스 메서드 수행 이후에 호출되는 로그");
+		// System.out.println("[ARA 로그] 비즈니스 메서드 수행 이후에 호출되는 로그");
 		
 		System.out.println(jp.getSignature().getName()+"()");
 		System.out.println(jp.getArgs()[0]);
